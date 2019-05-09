c91ffcdd87fdbf1110e2ef158c9c97a712d4f50a
mgalliou <matthias.galliou@gmail.com>
2019-05-09 22:45:52 +0200

    init next in lstnewnocpy

--- src/ft_lstnewnocpy.c
+++ src/ft_lstnewnocpy.c
@@ -6,7 +6,7 @@
 /*   By: mgalliou <mgalliou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 16:24:47 by mgalliou          #+#    #+#             */
-/*   Updated: 2018/03/06 16:29:30 by mgalliou         ###   ########.fr       */
+/*   Updated: 2019/05/09 22:07:05 by mgalliou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ t_list		*ft_lstnewnocpy(void *content, size_t content_size)
 		return (NULL);
 	new->content = content;
 	new->content_size = content_size;
+	new->next = NULL;
 	return (new);
 }
